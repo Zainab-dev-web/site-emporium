e70c77149fb1fdf04b077a206666e7118da11f60
zainab <zainabfahem96@gmail.com>
2019-12-17 17:00:10 +0100

    commit tache

--- js/index.js
+++ js/index.js
@@ -1,8 +1,37 @@
-// let themeblanc=document.getElementsByClassName('carre')[0]
-// let themenoir=document.getElementsByClassName('carre')[1]
+// Déclaration des variables
 
 
-// faire le carousel en js
+let themeblanc=document.getElementsByClassName('carre')[0]
+let themenoir=document.getElementsByClassName('carre')[1]
+let body=document.getElementsByTagName('body')
+let nav=document.getElementsByClassName('nav')
+
+// Ecouteur d'evenement des themes
+
+themenoir.addEventListener('click' , event=>{
+    body.style.backgroundColor="#"
+    nav.style.backgroundColor="#"
+})
+
+themeblanc.addEventListener('click', event=>{
+    body.style.backgroundColor="#"
+    nav.style.backgroundColor="#"
+})
 
 
-quand je clqiue sur le burger il affiche
+// Carousel en js ( se baser sur la video)
+
+// Nav ( refaire la nav voir sur )
+// quand je clique sur le burger il affiche
+
+
+
+
+
+// responsive
+
+// voir comment j'ai avec le site du chef ou chercher sur slack
+
+// Popup
+
+// faire comme j'ai fait pop up
