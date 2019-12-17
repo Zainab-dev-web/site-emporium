f66c5b34d3af3a7da79bf1e660df7d8d2a225b24
zainab <zainabfahem96@gmail.com>
2019-12-17 15:52:43 +0100

    commit debut carousel

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// let themeblanc=document.getElementsByClassName('carre')[0]
+// let themenoir=document.getElementsByClassName('carre')[1]
+
+
+// faire le carousel en js
+
+
+quand je clqiue sur le burger il affiche
